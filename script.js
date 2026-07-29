e2a12dfa61c47a983b988e88bdee2578ec5555df
Tastyandcomfort <muralimanohar.in@gmail.com>
2026-07-29 09:42:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -511,10 +511,10 @@ const videoPlaylist = [
   { type: "local", src: "video_240p (1).mp4" },
   { type: "local", src: "video_240p (3).mp4" },
   { type: "youtube", src: "https://www.youtube.com/embed/SF7htyffezs?autoplay=1&mute=1" },
-  { type: "youtube", src: "https://www.youtube.com/embed/wWEnxWA7nnY?autoplay=1&mute=1" }
+  { type: "youtube", src: "https://www.youtube.com/embed/OUBslrCqREs?autoplay=1&mute=1" }
 ];
 
-
+// https://www.youtube.com/live/OUBslrCqREs?si=C09raQSwy8dH7_H1
 // https://www.youtube.com/live/wWEnxWA7nnY?si=kgRkz2EkTnmvuO2h
 // https://youtu.be/SF7htyffezs?si=g1IeK_xKH09yft8b
 
