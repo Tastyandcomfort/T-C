fc3c840c550e39cf881e7e70dea6efad3b245fae
Tastyandcomfort <muralimanohar.in@gmail.com>
2026-08-04 10:55:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -710,10 +710,12 @@ const stallLng = 78.537016;
 
 // 2. Pre-defined amenities using exact coordinates to prevent search errors
 const customAmenities = {
-  'hospital': { name: 'Local Area Hospital', query: '17.369313, 78.536573' }, 
-  'police station': { name: 'Neighborhood Police Station', query: '17.368289, 78.527748' }, 
-  'metro station': { name: 'Nearest Metro Station', query: '17.368346, 78.536100' }, 
-  'bus station': { name: 'Local Bus Stop', query: '17.36915, 78.52842' }
+  'hospital': { name: 'Near Hospital', query: '17.369313, 78.536573' }, 
+  'police station': { name: 'Near Police Station', query: '17.368289, 78.527748' }, 
+  'metro station': { name: 'Near Metro Station', query: '17.368346, 78.536100' }, 
+  'bus station': { name: 'Near Bus Stop', query: '17.368161, 78.534952' },
+  'atm': { name: 'Near by 24/7 ATMs', query: '17.368727, 78.533186' },
+  'gas station': { name: 'Near fuel bunk', query: '17.367117, 78.541359' }
 };
 
 let allModesData = { driving: [], walking: [] };
