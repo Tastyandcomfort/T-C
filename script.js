ad78df1d12003b4b5684c1fe62083d045b1ac1cc
Tastyandcomfort <muralimanohar.in@gmail.com>
2026-08-28 22:16:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -388,7 +388,7 @@ function setMode(mode, element) {
             title.innerText = "Live NEWS";
             dot.style.display = "inline-block";
             views.video.style.display = 'block';
-            players.news.src = "https://www.youtube.com/embed/e1FIApIafWE?autoplay=1&mute=1";
+            players.news.src = "https://www.youtube.com/embed/GotlA1KKWoo?autoplay=1&mute=1";
         } 
         else if (mode === 'live') {
             title.innerText = "T&C Live";
@@ -469,7 +469,7 @@ function setMode(mode, element) {
 // https://www.youtube.com/live/QCwq5Lg7Xes?si=Zer6F4zIFHC9Z6SR
 // https://www.youtube.com/live/e1FIApIafWE?si=ZtVRb0WntF9uYHZx
 // https://youtu.be/-aTWDnQttks?si=SBTx1z6N_MK20dUw
-
+// https://www.youtube.com/live/GotlA1KKWoo?si=IgX1ZvDaUnDtLcm-
 
 
 
