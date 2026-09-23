fab26576a8dbf4e529aca09b640fbe4ae6c762c3
Tastyandcomfort <muralimanohar.in@gmail.com>
2026-09-23 11:17:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -388,7 +388,7 @@ function setMode(mode, element) {
             title.innerText = "Live NEWS";
             dot.style.display = "inline-block";
             views.video.style.display = 'block';
-            players.news.src = "https://www.youtube.com/embed/_jnDy56dehA?autoplay=1&mute=1";
+            players.news.src = "https://www.youtube.com/embed/II_m28Bm-iM?autoplay=1&mute=1";
         } 
         else if (mode === 'live') {
             title.innerText = "T&C Live";
@@ -470,7 +470,10 @@ function setMode(mode, element) {
 // https://www.youtube.com/live/e1FIApIafWE?si=ZtVRb0WntF9uYHZx
 // https://youtu.be/-aTWDnQttks?si=SBTx1z6N_MK20dUw
 // https://www.youtube.com/live/Gupg41GJlpo?si=tscBaxRnGJ5t4FPL
-https://www.youtube.com/live/_jnDy56dehA?si=aTz02-wbLsCzXPDJ
+//https://www.youtube.com/live/_jnDy56dehA?si=aTz02-wbLsCzXPDJ
+//https://www.youtube.com/live/II_m28Bm-iM?si=h5Z4hs2eCcPga8F2
+
+
 
 // Selector bar of videos
 function scrollToActiveTab(buttonElement) {
